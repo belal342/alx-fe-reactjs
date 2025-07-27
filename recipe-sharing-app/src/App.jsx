d60a7f3b3c894e1c3b2d762a0b6cb5ed8378c6a8
belal <blalsmyr512@gmail.com>
2025-07-27 20:22:25 +0300

    first with zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link, Outlet } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
+import EditRecipeForm from './components/EditRecipeForm'
 import NotFound from './components/NotFound'
 import './App.css'
 
@@ -10,7 +11,7 @@ function App() {
     <div className="app">
       <nav className="app-nav">
         <Link to="/" className="nav-link">All Recipes</Link>
-        <Link to="/add" className="nav-link">Add New Recipe</Link>
+        <Link to="/add" className="nav-link">Add Recipe</Link>
       </nav>
 
       <main className="app-main">
@@ -19,6 +20,7 @@ function App() {
             <Route index element={<RecipeList />} />
             <Route path="add" element={<AddRecipeForm />} />
             <Route path="recipes/:id" element={<RecipeDetails />} />
+            <Route path="recipes/:id/edit" element={<EditRecipeForm />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
@@ -30,7 +32,7 @@ function App() {
 function HomeLayout() {
   return (
     <div className="home-layout">
-      <h1>Recipe Sharing App</h1>
+      <h1>Recipe Sharing</h1>
       <Outlet />
     </div>
   )
